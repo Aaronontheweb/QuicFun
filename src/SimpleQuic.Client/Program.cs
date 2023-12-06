a1977fb84bf0b23f091ecb0730d2e56b756e6fed
Aaron Stannard <aaron@petabridge.com>
2023-12-05 23:02:39 -0500

    adding self-signed certificate

--- src/SimpleQuic.Client/Program.cs
+++ src/SimpleQuic.Client/Program.cs
@@ -1,35 +1,31 @@
-﻿
-using System.Buffers;
-using System.Buffers.Binary;
+﻿using System.Buffers.Binary;
 using System.Net;
 using System.Net.Quic;
 using System.Net.Security;
-using System.Threading.Channels;
-using Microsoft.VisualBasic;
 
-var quicConnection = await QuicConnection.ConnectAsync(new QuicClientConnectionOptions()
+var quicConnection = await QuicConnection.ConnectAsync(new QuicClientConnectionOptions
 {
     DefaultCloseErrorCode = 0x0B,
     DefaultStreamErrorCode = 0x0A,
     IdleTimeout = TimeSpan.FromSeconds(10),
     MaxInboundBidirectionalStreams = 10,
     RemoteEndPoint = new IPEndPoint(IPAddress.Loopback, 45505),
-    ClientAuthenticationOptions = new SslClientAuthenticationOptions()
+    ClientAuthenticationOptions = new SslClientAuthenticationOptions
     {
-        ApplicationProtocols = new List<SslApplicationProtocol>()
+        ApplicationProtocols = new List<SslApplicationProtocol>
         {
-            new SslApplicationProtocol("simplequic")
+            new("simplequic")
         },
+        RemoteCertificateValidationCallback = (sender, certificate, chain, errors) => true
     }
 });
 
 Console.WriteLine($"QUIC-Client: Connected to {quicConnection.RemoteEndPoint} via {quicConnection.LocalEndPoint}");
 
 const int MAX_ITEMS = 10_000;
-List<Task> CloseOps = new List<Task>();
+var CloseOps = new List<Task>();
 
 for (var i = 0; i < 10; i++)
-{
     try
     {
         var stream = await quicConnection.OpenOutboundStreamAsync(QuicStreamType.Bidirectional);
@@ -43,18 +39,20 @@ for (var i = 0; i < 10; i++)
             {
                 while (!stream.CanWrite)
                     await Task.Delay(20); // spin 20ms
-                
-                BinaryPrimitives.WriteInt32LittleEndian(writeBuffer, i);
+
+                BinaryPrimitives.WriteInt32LittleEndian(writeBuffer, c);
                 await stream.WriteAsync(writeBuffer);
             }
-            
+
             // complete writing the stream
             stream.CompleteWrites();
         }
 
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+#pragma warning disable CS4014
+        // Because this call is not awaited, execution of the current method continues before the call is completed
         BeginWritingChannel();
-#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+#pragma warning restore CS4014
+        // Because this call is not awaited, execution of the current method continues before the call is completed
 
         async Task BeginReadingChannel()
         {
@@ -66,21 +64,21 @@ for (var i = 0; i < 10; i++)
                 Console.WriteLine($"Received [{val}] on channel [{i}]");
                 curCount++;
             }
-            
+
             stream.Close();
         }
 
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+#pragma warning disable CS4014
+        // Because this call is not awaited, execution of the current method continues before the call is completed
         CloseOps.Add(BeginReadingChannel());
-#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+#pragma warning restore CS4014
+        // Because this call is not awaited, execution of the current method continues before the call is completed
     }
     catch (Exception ex)
     {
         Console.WriteLine($"Failed to open stream due to {ex.Message}");
         return;
     }
-}
 
 await Task.WhenAll(CloseOps);
 await quicConnection.CloseAsync(0x00);
-
--- src/SimpleQuic.Server/Program.cs
+++ src/SimpleQuic.Server/Program.cs
@@ -3,27 +3,33 @@
 using System.Net;
 using System.Net.Quic;
 using System.Net.Security;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 
-var quicListener = await QuicListener.ListenAsync(new QuicListenerOptions()
+var certificate = CreateSelfSignedCertificate();
+
+var quicListener = await QuicListener.ListenAsync(new QuicListenerOptions
 {
     ListenEndPoint = new IPEndPoint(IPAddress.Any, 45505),
-    ApplicationProtocols = new List<SslApplicationProtocol>()
+    ApplicationProtocols = new List<SslApplicationProtocol>
     {
-        new SslApplicationProtocol("simplequic")
+        new("simplequic")
     },
     ConnectionOptionsCallback = (connection, info, CancellationToken) => new ValueTask<QuicServerConnectionOptions>(
-        new QuicServerConnectionOptions()
+        new QuicServerConnectionOptions
         {
             DefaultCloseErrorCode = 0x0B,
             DefaultStreamErrorCode = 0x0A,
             IdleTimeout = TimeSpan.FromSeconds(10),
             MaxInboundBidirectionalStreams = 10,
-            ServerAuthenticationOptions = new SslServerAuthenticationOptions()
+            ServerAuthenticationOptions = new SslServerAuthenticationOptions
             {
-                ApplicationProtocols = new List<SslApplicationProtocol>()
+                ApplicationProtocols = new List<SslApplicationProtocol>
                 {
-                  new SslApplicationProtocol("simplequic")   
-                }
+                    new("simplequic")
+                },
+                ServerCertificate = certificate,
+                ClientCertificateRequired = false
             }
         })
 });
@@ -53,14 +59,17 @@ while (true)
             } while (!openStreams.TrueForAll(c => c.IsCompleted));
 
             await newConnection.CloseAsync(0x00);
-            await newConnection.DisposeAsync();
         }
         catch
         {
             // connection might have been closed
-            
         }
-        
+        finally
+        {
+            await newConnection.DisposeAsync();
+        }
+
+        Console.WriteLine($"QUIC-Server: connection from [{newConnection.RemoteEndPoint}] closed.");
     }
 
     async Task ProcessStream(QuicStream stream)
@@ -75,4 +84,51 @@ while (true)
         stream.CompleteWrites();
         stream.Close();
     }
+}
+
+// From https://github.com/dotnet/runtime/issues/92669#issuecomment-1737844302
+static X509Certificate2 CreateSelfSignedCertificate()
+{
+    using var rsa = RSA.Create();
+    var certificateRequest =
+        new CertificateRequest("CN=localhost", rsa, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+    certificateRequest.CertificateExtensions.Add(
+        new X509BasicConstraintsExtension(
+            false,
+            false,
+            0,
+            true
+        )
+    );
+    certificateRequest.CertificateExtensions.Add(
+        new X509KeyUsageExtension(
+            X509KeyUsageFlags.DigitalSignature | X509KeyUsageFlags.KeyEncipherment |
+            X509KeyUsageFlags.CrlSign | X509KeyUsageFlags.KeyCertSign,
+            false
+        )
+    );
+    certificateRequest.CertificateExtensions.Add(
+        new X509EnhancedKeyUsageExtension(
+            new OidCollection
+            {
+                //new Oid("1.3.6.1.5.5.7.3.2"), // TLS Client auth
+                new("1.3.6.1.5.5.7.3.1") // TLS Server auth
+            },
+            false));
+
+    certificateRequest.CertificateExtensions.Add(
+        new X509SubjectKeyIdentifierExtension(
+            certificateRequest.PublicKey,
+            false
+        )
+    );
+
+    var sanBuilder = new SubjectAlternativeNameBuilder();
+    sanBuilder.AddDnsName("localhost");
+    certificateRequest.CertificateExtensions.Add(sanBuilder.Build());
+
+    var cert = certificateRequest.CreateSelfSigned(DateTimeOffset.Now.AddDays(-1), DateTimeOffset.Now.AddYears(5));
+
+    // windows only
+    return new X509Certificate2(cert.Export(X509ContentType.Pfx), (string?)null, X509KeyStorageFlags.Exportable);
 }
